41f7e192f636d402ea8961966bc023b31baa7c41
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-10-23 21:18:37 +0600

    Создал страницу детального просмотра

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from webapp.views import article_list_view
+from webapp.views import article_list_view, article_detail_view
 
 urlpatterns = [
     path('', article_list_view),
+    path('article/', article_detail_view)
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from webapp.models import Article
@@ -9,3 +10,11 @@ def article_list_view(request):
         'articles': articles
     }
     return render(request, 'article_list.html', context)
+
+def article_detail_view(request):
+    article_id = request.GET.get('id')
+    if article_id:
+        article = Article.objects.get(id=article_id)
+        return render(request, 'article_detail.html', {'article': article})
+    else:
+        return HttpResponseRedirect('/')

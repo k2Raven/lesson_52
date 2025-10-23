b5760d1a3c1bc1c084629056141077b83854a5f1
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-10-23 20:15:36 +0600

    создал модель Статей

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50,
+                             null=False,
+                             blank=False,
+                             verbose_name='Заголовок')
+    content = models.TextField(verbose_name='Контент')
+    author = models.CharField(max_length=50, default='Anonymous', verbose_name='Автор')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+

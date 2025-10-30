00e9d11ccafb8c2c6e385741f844e6d3ea8f659f
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-10-30 20:05:48 +0600

    добавил модель status и связал с article

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from webapp.models import Article
+from webapp.models import Article, Status
+
+admin.site.register(Status)
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'author', 'created_at', 'updated_at')
+    list_display = ('id', 'title', 'author','status', 'created_at', 'updated_at')
     list_filter = ['author']
     search_fields = ('title', 'content')
-    fields = ('title', 'content', 'author', 'created_at', 'updated_at')
+    fields = ('title', 'content', 'author', 'status', 'created_at', 'updated_at')
     readonly_fields = ('created_at', 'updated_at')
--- webapp/models.py
+++ webapp/models.py
@@ -1,5 +1,17 @@
 from django.db import models
-status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'),  ('done', 'Сделано')]
+
+status_choices = [('new', 'Новая'), ('in_progress', 'В процессе'), ('done', 'Сделано')]
+
+
+class Status(models.Model):
+    title = models.CharField(max_length=20, verbose_name='Название')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
 class Article(models.Model):
     title = models.CharField(max_length=50,
                              null=False,
@@ -7,9 +19,10 @@ class Article(models.Model):
                              verbose_name='Заголовок')
     content = models.TextField(verbose_name='Контент')
     author = models.CharField(max_length=50, default='Anonymous', verbose_name='Автор')
+    status = models.ForeignKey('webapp.Status', verbose_name='Статус', on_delete=models.RESTRICT,
+                               related_name='articles', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return f'{self.id} - {self.title}'
-

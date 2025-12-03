5196506f8953e92f208f70a89f6c50c3ecd3994e
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-12-03 19:33:29 +0600

    реализовал смену пароля

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView, LoginView
-from accounts.views import RegisterView, UserDetailView, UserChangeView
+from accounts.views import RegisterView, UserDetailView, UserChangeView, UserPasswordChangeView
 
 app_name = 'accounts'
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('profile/<int:pk>/', UserDetailView.as_view(), name='profile'),
     path('profile/<int:pk>/edit/', UserChangeView.as_view(), name='profile_edit'),
+    path('profile/password/', UserPasswordChangeView.as_view(), name='password_change'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,15 @@
+from django.contrib.auth.views import PasswordChangeView
 from django.core.paginator import Paginator
 from django.urls import reverse
 from django.views.generic import CreateView, DetailView, UpdateView
 from django.contrib.auth import get_user_model, login
 from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from accounts.forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm
 from accounts.models import Profile
 
 User = get_user_model()
+
 
 class RegisterView(CreateView):
     template_name = 'user_create.html'
@@ -27,11 +30,11 @@ class RegisterView(CreateView):
             next = reverse('webapp:article_list')
         return next
 
+
 class UserDetailView(DetailView):
     model = User
     template_name = 'user_detail.html'
     context_object_name = 'user_obj'
-
 
     def get_context_data(self, **kwargs):
         articles = self.object.articles.order_by('-created_at')
@@ -43,11 +46,15 @@ class UserDetailView(DetailView):
         kwargs['is_paginated'] = page.has_other_pages()
         return super().get_context_data(**kwargs)
 
-class UserChangeView(UpdateView):
+
+class UserChangeView(UserPassesTestMixin, UpdateView):
     model = User
     form_class = UserChangeForm
     template_name = 'user_change.html'
     context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
 
     def get_profile_form(self):
         form_kwargs = {'instance': self.object.profile}
@@ -55,7 +62,6 @@ class UserChangeView(UpdateView):
             form_kwargs['data'] = self.request.POST
             form_kwargs['files'] = self.request.FILES
         return ProfileChangeForm(**form_kwargs)
-
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -75,3 +81,9 @@ class UserChangeView(UpdateView):
             context = self.get_context_data(form=form, profile_form=profile_form)
             return self.render_to_response(context)
 
+
+class UserPasswordChangeView(LoginRequiredMixin, PasswordChangeView):
+    template_name = 'user_password_change.html'
+
+    def get_success_url(self):
+        return reverse('accounts:profile', kwargs={'pk': self.request.user.pk})

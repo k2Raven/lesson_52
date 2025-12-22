d59a158001265acfc5fb49b8428d472104f1db19
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-12-22 21:09:17 +0600

    добавил endpoints для получения CSRF токена, echo запроса и списка статей

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
 
+from api_v1.views import JsonEchoView, get_token_view, ArticleView
+
 urlpatterns = [
+    path('get_token/', get_token_view),
+    path('echo/', JsonEchoView.as_view()),
+    path('article/', ArticleView.as_view()),
 ]
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import json
+from datetime import datetime
+from http import HTTPStatus
 
-# Create your views here.
+from django.core.serializers import serialize
+from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
+from django.views import View
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+from webapp.models import Article
+
+
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+class JsonEchoView(View):
+    def get(self, request, *args, **kwargs):
+        answer = {
+            'time': f'{datetime.now():%Y-%m-%d %H:%M:%S}',
+            'method': 'GET'
+        }
+        if request.body:
+            answer['body'] = json.loads(request.body)
+        return JsonResponse(answer)
+
+    def post(self, request, *args, **kwargs):
+        answer = {
+            'time': f'{datetime.now():%Y-%m-%d %H:%M:%S}',
+            'method': 'POST'
+        }
+        if request.body:
+            answer['body'] = json.loads(request.body)
+        return JsonResponse(answer, status=HTTPStatus.OK)
+
+class ArticleView(View):
+    def get(self, request, *args, **kwargs):
+        # articles = Article.objects.values('id', 'title', 'author__username')
+        articles = Article.objects.all()
+        articles = serialize('json', articles)
+        # return JsonResponse(articles, safe=False)
+        return HttpResponse(articles, content_type='application/json')
